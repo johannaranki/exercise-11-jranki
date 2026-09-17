7bea726ded79bec343eaae02cb2591555db01ca6
johannaranki <rankijohanna@gmail.com>
2026-09-17 14:34:54 +0300

    Answers and functionality added

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,12 @@
 /* Funktiot ja muu Javascript */
+
+document.getElementById("showAnswers").addEventListener("click", clickButton)
+
+function clickButton() {
+  const answers = document.querySelectorAll(".hide")
+
+  for (const answer of answers) {
+    answer.classList.remove("hide")
+    answer.classList.add("show")
+  }
+}
